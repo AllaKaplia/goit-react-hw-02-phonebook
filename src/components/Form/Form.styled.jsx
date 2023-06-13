2267919ffd1d03d50384ccbd82e1dae68e53c00d
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-14 00:10:50 +0300

    Create Form.styled.jsx

---
+++ src/components/Form/Form.styled.jsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { Field } from 'formik';
+
+export const Input = styled(Field)`
+    padding: 5px;
+    border: 2px solid greenyellow;
+    border-radius: 5px;
+`;
